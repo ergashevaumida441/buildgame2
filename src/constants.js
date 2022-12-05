d901a4b1e999b97b2a1da9acaf8e848141f2edc8
Umida Ergasheva <117135080+ergashevaumida441@users.noreply.github.com>
2022-12-05 08:42:33 +0000

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0xEFDE64D57495026bF71361c3f616affb6d45648a';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
